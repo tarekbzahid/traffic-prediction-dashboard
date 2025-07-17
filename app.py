aa084fd8ef2c851cd0d04ec0c7a09cd82a035075
Tarek Z <bonie_tg@outlook.com>
2025-07-17 02:00:05 -0700

    Update app.py

--- app.py
+++ app.py
@@ -134,4 +134,4 @@ def config():
 # ----------------------------
 if __name__ == "__main__":
     socketio.start_background_task(fetch_live_data)
-    socketio.run(app, host="127.0.0.1", port=5000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
